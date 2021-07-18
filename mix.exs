26cc7d36715d896facc06f89b6526ee622293423
José Valim <jose.valim@dashbit.co>
2021-07-19 01:29:52 +0200

    Release v1.0.0

--- mix.exs
+++ mix.exs
@@ -1,12 +1,16 @@
 defmodule PhoenixView.MixProject do
   use Mix.Project
 
+  @version "1.0.0"
+  @source_url "https://github.com/phoenixframework/phoenix_view"
+
   def project do
     [
       app: :phoenix_view,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.9",
-      start_permanent: Mix.env() == :prod,
+      description: "Views and template rendering for Phoenix",
+      docs: docs(),
       deps: deps(),
       package: package()
     ]
@@ -14,15 +18,23 @@ defmodule PhoenixView.MixProject do
 
   def application do
     [
-      extra_applications: [:eex],
+      extra_applications: [:eex]
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Phoenix.View",
+      source_url: @source_url,
+      source_ref: "v#{@version}"
     ]
   end
 
   defp deps do
     [
-      {:jason, "~> 1.0", optional: true},
-      {:phoenix_html, "~> 2.14.2 or ~> 2.15"},
-      {:ex_doc, "~> 0.22", only: :docs},
+      {:phoenix_html, "~> 2.14.2 or ~> 3.0", optional: true},
+      {:jason, "~> 1.0", only: :test},
+      {:ex_doc, "~> 0.22", only: :docs}
     ]
   end
 
@@ -31,8 +43,7 @@ defmodule PhoenixView.MixProject do
       maintainers: ["Chris McCord", "José Valim", "Gary Rennie"],
       licenses: ["MIT"],
       links: %{github: "https://github.com/phoenixframework/phoenix_view"},
-      files:
-      ~w(lib mix.exs .formatter.exs)
+      files: ~w(lib mix.exs .formatter.exs)
     ]
   end
 end
